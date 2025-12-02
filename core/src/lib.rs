b114e46c4bdf568c2bbff1e76018346e4ed8c654
foldr <daniel@veridise.com>
2025-12-02 11:05:13 +0100

    Core crate (#1)
    * Add ignore
    
    * Rust format
    
    * LICENSE file
    
    * basic core crate
    
    * Core crate

---
+++ core/src/lib.rs
@@ -0,0 +1,12 @@
+#![doc = include_str!("../README.md")]
+#![deny(rustdoc::broken_intra_doc_links)]
+#![deny(missing_debug_implementations)]
+#![deny(missing_docs)]
+
+pub mod error;
+pub mod expressions;
+pub mod info_traits;
+pub mod lookups;
+pub mod query;
+pub mod synthesis;
+pub mod table;
